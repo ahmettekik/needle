e5331f2f295748742dd5fb2a729c8adcc4ab7cfb
ahmettekik <ahmetttekik@gmail.com>
2015-09-02 10:49:34 -0700

    first commit

---
+++ app/src/main/java/com/example/android/needle/LoginFragment.java
@@ -0,0 +1,23 @@
+package com.example.android.needle;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jonfisk on 31/08/15.
+ */
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        return rootView;
+    }
+}
